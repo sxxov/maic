f59d11953305b4a39f7333d2e01ec0783298b820
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-11-15 17:44:12 +0800

    Correctly sync version with upstream

--- publish.ts
+++ publish.ts
@@ -9,7 +9,10 @@ const thisDir = dirname(thisPath);
 const pkg = JSON.parse(
 	readFile(join(thisDir, './package.json'), 'utf8'),
 ) as typeof import('./package.json');
-const iconVersion = pkg.devDependencies['@material-design-icons/svg'];
+const iconVersion = pkg.devDependencies['@material-design-icons/svg'].replace(
+	/^\^/,
+	'',
+);
 
 console.log(`publish: setting version(${iconVersion})...`);
 execSync(`npm version ${iconVersion}`);
